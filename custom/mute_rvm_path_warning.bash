08016854b13746935771208e7c26323ee206691b
Johannes Tuchscherer <jtuchscherer@pivotal.io>
2015-12-31 12:13:16 -0700

    Fixing the rvrmc file PATH warning squelcher
    Signed-off-by: Johannes Tuchscherer <jtuchscherer@pivotal.io>

--- custom/mute_rvm_path_warning.bash
+++ custom/mute_rvm_path_warning.bash
@@ -1,3 +1,5 @@
-!/usr/bin/env bash
-
-echo 'rvm_silence_path_mismatch_check_flag=1' >> ~/.rvmrc
+#!/bin/bash
+if ! grep -Fxq "rvm_silence_path_mismatch_check_flag=1" ~/.rvmrc
+then
+    echo 'rvm_silence_path_mismatch_check_flag=1' >> ~/.rvmrc    
+fi
