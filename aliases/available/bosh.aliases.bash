fa16080ac4d4f15ab1416b9dc12b489067307fbf
Trevor Whitney <twhitney@pivotal.io>
2018-01-09 12:12:53 -0700

    fix bug in bosh aliases

--- aliases/available/bosh.aliases.bash
+++ aliases/available/bosh.aliases.bash
@@ -1,6 +1,6 @@
 cite 'about-alias'
 about-alias 'common bosh abbreviations'
 
-alias bosh-dev="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch-dev > /dev/null; head -n3 <(/usr/local/bin/bbl print-env); popd > /dev/null"
-alias bosh-acceptance="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch/ > /dev/null; head -n3 <(/usr/local/bin/bbl print-env); popd > /dev/null"
+alias bosh-dev="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch-dev > /dev/null; /usr/local/bin/bbl print-env; popd > /dev/null"
+alias bosh-acceptance="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch/ > /dev/null; /usr/local/bin/bbl print-env; popd > /dev/null"
                                                                                                                                         s
