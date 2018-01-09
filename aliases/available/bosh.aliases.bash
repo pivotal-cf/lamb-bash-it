36c83e3c0d5bb2a02706fed7c99d452ddb65ac78
Trevor Whitney <twhitney@pivotal.io>
2018-01-09 10:13:22 -0700

    remove non-working bosh aliases

--- aliases/available/bosh.aliases.bash
+++
@@ -1,6 +0,0 @@
-cite 'about-alias'
-about-alias 'common bosh abbreviations'
-
-alias bosh-dev="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch-dev/ > /dev/null; bbl print-env; popd > /dev/null"
-alias bosh-acceptance="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch/ > /dev/null; bbl print-env; popd > /dev/null"
-                                                                                                                                        s
