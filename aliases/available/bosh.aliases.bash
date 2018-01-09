d36d322aacbcd27902be349bd6930a0a6221ebc8
Trevor Whitney <twhitney@pivotal.io>
2018-01-09 10:07:52 -0700

    add bosh-dev and bosh-acceptance aliases

---
+++ aliases/available/bosh.aliases.bash
@@ -0,0 +1,6 @@
+cite 'about-alias'
+about-alias 'common bosh abbreviations'
+
+alias bosh-dev="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch-dev/ > /dev/null; bbl print-env; popd > /dev/null"
+alias bosh-acceptance="pushd ~/workspace/deployments-metrics/bosh-deployments/gcp/healthwatch/ > /dev/null; bbl print-env; popd > /dev/null"
+                                                                                                                                        s
