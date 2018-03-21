60fb1c4ebd207eacffcaab758e337f966ef83621
Glenn Oppegard <goppegard@pivotal.io>
2018-03-21 14:53:21 -0600

    Fix healthwatch aliases

--- custom/healthwatch.bash
+++ custom/healthwatch.bash
@@ -1,5 +1,6 @@
-alias gdf=“git diff --color | diff-so-fancy”
-alias healthwatch-data=“cd ~/workspace/go/src/github.com/pivotal-cf/healthwatch-data/java”
-alias release-data=“cd ~/workspace/healthwatch-release/src/github.com/pivotal-cf/healthwatch-data”
-alias release=“cd ~/workspace/healthwatch-release”
-alias tile=“cd ~/workspace/p-healthwatch”
+alias gap="git add -p"
+alias gdf="git diff --color | diff-so-fancy"
+alias healthwatch-data="cd ~/workspace/go/src/github.com/pivotal-cf/healthwatch-data/java"
+alias release-data="cd ~/workspace/healthwatch-release/src/github.com/pivotal-cf/healthwatch-data"
+alias release="cd ~/workspace/healthwatch-release"
+alias tile="cd ~/workspace/p-healthwatch"
